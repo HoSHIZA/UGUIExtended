a4ddc38f3092e874b13d1efc107b76bb121a2461
HoSHIZA <58137717+HoSHIZA@users.noreply.github.com>
2023-07-07 21:22:56 +0300

    Update DraggableRectHandler.cs
    Fix namespace

--- Code/Runtime/Components/Drag/DraggableRectHandler.cs
+++ Code/Runtime/Components/Drag/DraggableRectHandler.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-namespace KDebugger.Plugins.ShizoGames.ImprovedUI.Components.Drag
+namespace ShizoGames.ImprovedUI.Components.Drag
 {
     [DisallowMultipleComponent]
     public sealed class DraggableRectHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
