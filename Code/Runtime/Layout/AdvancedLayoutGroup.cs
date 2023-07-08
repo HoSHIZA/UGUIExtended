75efaa3a9fb0fa3b86502bccd5dd1805c9f85ff7
HoSHIZA <needahentai@gmail.com>
2023-07-08 22:22:26 +0300

    Added AdvancedLayoutGroup

---
+++ Code/Runtime/Layout/AdvancedLayoutGroup.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ShizoGames.UGUIExtended.Layout
+{
+    [AddComponentMenu(PackageConfig.ADD_COMPONENT_UI_ROOT + "Layout/Advanced Size Fitter")]
+    public class AdvancedLayoutGroup : HorizontalOrVerticalLayoutGroup
+    {
+        [SerializeField] private LayoutGroupType _type;
+
+        private bool IsVertical => _type == LayoutGroupType.Vertical;
+        private bool IsHorizontal => _type == LayoutGroupType.Horizontal;
+        
+        protected AdvancedLayoutGroup()
+        {}
+        
+        public override void CalculateLayoutInputHorizontal()
+        {
+            if (_type == LayoutGroupType.Disabled) return;
+            
+            base.CalculateLayoutInputHorizontal();
+            
+            CalcAlongAxis(0, true);
+        }
+        
+        public override void CalculateLayoutInputVertical()
+        {
+            if (_type == LayoutGroupType.Disabled) return;
+            
+            CalcAlongAxis(1, true);
+        }
+        
+        public override void SetLayoutHorizontal()
+        {
+            if (_type == LayoutGroupType.Disabled) return;
+            
+            SetChildrenAlongAxis(0, true);
+        }
+        
+        public override void SetLayoutVertical()
+        {
+            if (_type == LayoutGroupType.Disabled) return;
+            
+            SetChildrenAlongAxis(1, true);
+        }
+        
+        private enum LayoutGroupType
+        {
+            Disabled,
+            Vertical,
+            Horizontal,
+        }
+    }
+}
